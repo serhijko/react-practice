b0ef633681a55d2186a7eb270bce46b3db6e56be
serhijko <sp_by@tut.by>
2020-11-14 11:01:50 -0300

    Routers creation

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { IndexRoute, Route } from 'react-router';
 import App from './app';
+import { HomePage, HomeRoutes } from './pages/home';
+import { ContactRoutes } from './pages/contact';
 
 export default (
-    <Route component={ App } path={ App.path }></Route>
+    <Route component={ App } path={ App.path }>
+        <IndexRoute component={ HomePage } />
+
+        { HomeRoutes }
+        { ContactRoutes }
+    </Route>
 );
