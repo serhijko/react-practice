ead728533d785d694b45069ca9f8cee45d0aa159
serhijko <sp_by@tut.by>
2020-11-14 03:28:09 -0300

    Redux configuration

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import rootReducer from './reducers';
+
+
+function _applyMiddleware() {
+    const middleware = [];
+
+    return applyMiddleware(...middleware);
+}
+
+export default function configureStore(initialState) {
+    const store = compose(
+        _applyMiddleware()
+    )(createStore)(rootReducer, initialState);
+
+    return store;
+}
