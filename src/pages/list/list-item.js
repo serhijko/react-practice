fbd1cc1bd563792d2eedc1a9ece09f1f9beefdce
serhijko <sp_by@tut.by>
2020-11-23 17:45:43 -0300

    Detail view page

--- src/pages/list/list-item.js
+++ src/pages/list/list-item.js
@@ -4,14 +4,24 @@ import { Link } from 'react-router';
 export default class ListItem extends React.Component {
 
     static propTypes = {
-        id: PropTypes.number.isRequired
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired
     };
 
     render() {
         return (
-            <div>
-                <Link to={`/list/${ this.props.id }`}>Item { this.props.id }</Link>
-            </div>
+            <tr>
+                <td>{ this.props.id }</td>
+                <td><Link to={`/list/${ this.props.id }`}>{ this.props.name }</Link></td>
+                <td>
+                    <button className='btn btn-success'>
+                        <i className='glyphicon glyphicon-pencil' />
+                    </button>
+                    <button className='btn btn-danger'>
+                        <i className='glyphicon glyphicon-remove' />
+                    </button>
+                </td>
+            </tr>
         );
     }
 
--- src/reducers.js
+++ src/reducers.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import { HomeReducer } from './pages/home';
+import { ListReducer } from './pages/list';
 
 
 export default combineReducers({
     routing: routerReducer,
-    ...HomeReducer
+    ...HomeReducer,
+    ...ListReducer
 });
